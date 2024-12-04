957990cf6a9855b1147264a57875966fdd4cce89
tanishqmyadam <tanishqmyadam2003@gmail.com>
2024-12-04 00:19:09 -0600

    I have no clue if this works please

--- src/app/api/test-env/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from 'next/server';
-
-export async function GET() {
-  return NextResponse.json({ 
-    hasCloudName: !!process.env.CLOUDINARY_CLOUD_NAME,
-    hasApiKey: !!process.env.CLOUDINARY_API_KEY,
-    hasApiSecret: !!process.env.CLOUDINARY_API_SECRET
-  });
-} 
